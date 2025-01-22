5b71db3d5274f3710570e507346e2f2f4d1fa0c6
Interesting <1216744673@qq.com>
2025-01-22 19:04:06 +0800

    事件分发器

--- Nazel/src/Nazel/Events/Event.h
+++ Nazel/src/Nazel/Events/Event.h
@@ -50,13 +50,13 @@ protected:
 class EventDispatcher
 {
 	template <typename T>
-	using EventFn = std::function<bool>(T&);
+	using EventFn = std::function<bool(T&)>;
 public:
 	EventDispatcher(Event& event) : m_Event(event) { }
 
 	template <typename T>
 	bool Dispatch(EventFn<T> func) {
-		if (m_Event.GetEventType() == T.GetStaticType()) {
+		if (m_Event.GetEventType() == T::GetStaticType()) {
 			m_Event.m_Handled = func(*(T*)&m_Event);
 			return true;
 		}
