bd271a910a552aa4bc496d785ef21f2c4e493329
Interesting <1216744673@qq.com>
2025-02-14 19:01:46 +0800

    添加imgui docking模式，链接未通过需要排查

--- Nazel/src/Nazel/imgui/ImguiLayer.h
+++ Nazel/src/Nazel/imgui/ImguiLayer.h
@@ -10,19 +10,13 @@ class NAZEL_API ImGuiLayer : public Layer
 public:
 	ImGuiLayer();
 	~ImGuiLayer();
-	void OnAttch() override;
-	void OnDetach() override;
-	void OnUpdate() override;
-	void OnEvent(Event& event) override;
-private:
-	bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
-	bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
-	bool OnMouseMovedEvent(MouseMovedEvent& e);
-	bool OnMouseScrolledEvent(MouseScrolledEvent& e);
-	bool OnKeyPressedEvent(KeyPressedEvent& e);
-	bool OnKeyReleasedEvent(KeyReleasedEvent& e);
-	bool OnKeyTypedEvent(KeyTypedEvent& e);
-	bool OnWindowsResizeEvent(WindowResizeEvent& e);
+
+	void Begin();
+	void End();
+
+	virtual void OnAttach() override;
+	virtual void OnDetach() override;
+	virtual void OnImGuiRender() override;
 
 private:
 	float m_Time = 0.0f;
