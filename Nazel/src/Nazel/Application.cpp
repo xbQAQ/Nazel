a9f19cf33e49155756d110aa2b72bd9ea8b9d37d
Interesting <1216744673@qq.com>
2025-02-19 12:08:46 +0800

    添加VertexArray -- OpenGL

--- Nazel/src/Nazel/Application.cpp
+++ Nazel/src/Nazel/Application.cpp
@@ -9,23 +9,6 @@ namespace Nazel {
 #define BIND_EVENT_FUNCTION(X) std::bind(&Application::X, this, std::placeholders::_1)
 Application* Application::s_Instance = nullptr;
 
-static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type) {
-	switch (type) {
-	case ShaderDataType::Float:   return GL_FLOAT;
-	case ShaderDataType::Float2:  return GL_FLOAT;
-	case ShaderDataType::Float3:  return GL_FLOAT;
-	case ShaderDataType::Float4:  return GL_FLOAT;
-	case ShaderDataType::Mat3:    return GL_FLOAT;
-	case ShaderDataType::Mat4:    return GL_FLOAT;
-	case ShaderDataType::Int:     return GL_INT;
-	case ShaderDataType::Int2:    return GL_INT;
-	case ShaderDataType::Int3:    return GL_INT;
-	case ShaderDataType::Int4:    return GL_INT;
-	case ShaderDataType::Bool:    return GL_BOOL;
-	}
-	NZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
-	return 0;
-}
 
 Application::Application() {
 	m_Window = std::unique_ptr<Window>(Window::Create());
@@ -35,11 +18,8 @@ Application::Application() {
 	m_ImGuiLayer = new ImGuiLayer();
 	PushOverlay(m_ImGuiLayer);
 
-	// 0.生成顶点数组对象VAO、顶点缓冲对象VBO、索引缓冲对象EBO
-	// 
-	// 1. 绑定顶点数组对象
-	glGenVertexArrays(1, &m_VertexArray);
-	glBindVertexArray(m_VertexArray);
+	// 1. 创建顶点数组
+	m_VertexArray.reset(VertexArray::Create());
 
 	// 使用OpenGL函数渲染一个三角形
 	
@@ -50,34 +30,41 @@ Application::Application() {
 		 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
 		 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 	};
+	std::shared_ptr<VertexBuffer> m_VertexBuffer;
 	m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+	// 4. 设定顶点属性指针，来解释顶点缓冲中的顶点属性布局
+
+	BufferLayout layout = {
+		{ShaderDataType::Float3, "a_Position"},
+		{ShaderDataType::Float4, "a_Color" }
+	};
+	m_VertexBuffer->SetLayout(layout);
+	m_VertexArray->AddVertexBuffer(m_VertexBuffer);
 
 	// 3. 复制我们的CPU的索引数据到GPU索引缓冲中，供OpenGL使用
 	uint32_t indices[3] = { 0, 1, 2 }; // 索引数据
+	std::shared_ptr<IndexBuffer> m_IndexBuffer;
 	m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(unsigned int)));
-	
-	// 4. 设定顶点属性指针，来解释顶点缓冲中的顶点属性布局
+	m_VertexArray->SetIndexBuffer(m_IndexBuffer);
 
-	{
-		BufferLayout layout = {
-			{ShaderDataType::Float3, "a_Position"},
-			{ShaderDataType::Float4, "a_Color" }
-		};
-		m_VertexBuffer->SetLayout(layout);
-	}
+	m_SquareVA.reset(VertexArray::Create());
 
-	uint32_t index = 0;
-	const auto& layout = m_VertexBuffer->GetLayout();
-	for (const auto& element : layout) {
-		glEnableVertexAttribArray(index);
-		glVertexAttribPointer(index, 
-							  element.GetComponentCount(), 
-							  ShaderDataTypeToOpenGLBaseType(element.Type), 
-							  element.Normalized, 
-							  layout.GetStride(), 
-							  (const void*)element.Offset);
-		index++;
-	}
+	float squareVertices[3 * 4] = {
+		-0.75f, -0.75f, 0.0f,
+		 0.75f, -0.75f, 0.0f,
+		 0.75f,  0.75f, 0.0f,
+		-0.75f,  0.75f, 0.0f
+	};
+	std::shared_ptr<VertexBuffer> squareVB;
+	squareVB.reset(VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
+	squareVB->SetLayout({
+		{ ShaderDataType::Float3, "a_Position" }
+						});
+	m_SquareVA->AddVertexBuffer(squareVB);
+	uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
+	std::shared_ptr<IndexBuffer> squareIB;
+	squareIB.reset(IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
+	m_SquareVA->SetIndexBuffer(squareIB);
 
 	// 着色器代码
 	std::string vertexSrc = R"(
@@ -110,6 +97,29 @@ Application::Application() {
 		)";
 
 	m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
+
+	std::string blueShaderVertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+			out vec3 v_Position;
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = vec4(a_Position, 1.0);	
+			}
+		)";
+	std::string blueShaderFragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+			in vec3 v_Position;
+			void main()
+			{
+				color = vec4(0.2, 0.3, 0.8, 1.0);
+			}
+		)";
+	m_BlueShader.reset(new Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
 }
 
 Application::~Application() {
@@ -120,9 +130,13 @@ void Application::Run() {
 		glClearColor(1, 0, 1, 1);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		m_BlueShader->Bind();
+		m_SquareVA->Bind();
+		glDrawElements(GL_TRIANGLES, m_SquareVA->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+
 		m_Shader->Bind();
-		glBindVertexArray(m_VertexArray);
-		glDrawElements(GL_TRIANGLES, m_IndexBuffer->GetCount(), GL_UNSIGNED_INT, nullptr);
+		m_VertexArray->Bind();
+		glDrawElements(GL_TRIANGLES, m_VertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
 
 		for (Layer* layer : m_LayerStack)
 			layer->OnUpdate();
