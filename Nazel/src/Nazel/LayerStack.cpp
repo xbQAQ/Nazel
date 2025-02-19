6eb78df470f1df5bd4c6f44391211e48b0c56fa8
Interesting <1216744673@qq.com>
2025-02-19 13:59:19 +0800

    更新LayerStack，现在Pop时先Detach，然后再删除

--- Nazel/src/Nazel/LayerStack.cpp
+++ Nazel/src/Nazel/LayerStack.cpp
@@ -24,16 +24,19 @@ void LayerStack::PushOverlay(Layer* overlay) {
 }
 
 void LayerStack::PopLayer(Layer* layer) {
-	auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
+	// 搜索区域分为两部分，任何覆盖层都将被推到顶部，任何（其他）层将插入中间（在插入的层的顶部）。
+	auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
 	if (it != m_Layers.end()) {
+		layer->OnDetach();
 		m_Layers.erase(it);
 		m_LayerInsertIndex--;
 	}
 }
 
 void LayerStack::PopOverlay(Layer* overlay) {
-	auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
+	auto it = std::find(m_Layers.begin() + m_LayerInsertIndex, m_Layers.end(), overlay);
 	if (it != m_Layers.end())
+		overlay->OnDetach();
 		m_Layers.erase(it);
 }
 
