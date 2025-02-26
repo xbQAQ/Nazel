e9ced22276a77ee01807a306d7fc650b292a231a
Interesting <1216744673@qq.com>
2025-02-27 01:24:39 +0800

    格式化和更新premake

--- Nazel/src/Nazel/RenderAPI/Texture.cpp
+++ Nazel/src/Nazel/RenderAPI/Texture.cpp
@@ -6,14 +6,14 @@
 
 namespace Nazel {
 
-	Ref<Texture2D> Texture2D::Create(const std::string& path) {
-		switch (Renderer::GetAPI()) {
-		case RenderAPI::API::None:    NZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RenderAPI::API::OpenGL:  return std::make_shared<OpenGLTexture2D>(path);
-		}
-
-		NZ_CORE_ASSERT(false, "Unknown RendererAPI!");
-		return nullptr;
+Ref<Texture2D> Texture2D::Create(const std::string& path) {
+	switch (Renderer::GetAPI()) {
+	case RenderAPI::API::None:    NZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+	case RenderAPI::API::OpenGL:  return std::make_shared<OpenGLTexture2D>(path);
 	}
+
+	NZ_CORE_ASSERT(false, "Unknown RendererAPI!");
+	return nullptr;
+}
 
 }
