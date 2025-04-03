1065a6ab94de949a252ee431bb82d03e8901c81e
Interesting <1216744673@qq.com>
2025-04-03 17:51:54 +0800

    添加纹理数组，优化纹理batch --OpenGL

--- Nazel/src/Platform/OpenGL/OpenGLShader.h
+++ Nazel/src/Platform/OpenGL/OpenGLShader.h
@@ -20,6 +20,7 @@ public:
 	void SetFloat3(const std::string& name, const glm::vec3& value) override;
 	void SetFloat4(const std::string& name, const glm::vec4& value) override;
 	void SetMat4(const std::string& name, const glm::mat4& value) override;
+	void SetIntArray(const std::string& name, int* values, uint32_t count) override;
 
 	virtual const std::string& GetName() const override;
 
@@ -29,9 +30,9 @@ public:
 	void UploadUniformFloat2(const std::string& name, const glm::vec2& value);
 	void UploadUniformFloat3(const std::string& name, const glm::vec3& value);
 	void UploadUniformFloat4(const std::string& name, const glm::vec4& value);
-
 	void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
 	void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
+	void UploadUniformIntArray(const std::string& name, int* values, uint32_t count);
 private:
 	std::string ReadFile(const std::string& filepath);
 	std::unordered_map<GLenum, std::string> PreProcess(const std::string& source);
@@ -39,8 +40,6 @@ private:
 private:
 	uint32_t m_RendererID;
 	std::string m_Name;
-
-	
 };
 }
 
